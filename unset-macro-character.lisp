396411295fe6af851ac0497eeb0ef5fe565533dc
toshiabe <toshihisa.abe@gmail.com>
2011-12-02 00:27:21 +0900

    modify unset-macro-character.

--- unset-macro-character.lisp
+++ unset-macro-character.lisp
@@ -2,17 +2,16 @@
 ;;;; unset-macro-character.lisp - Unset macro character function from readtable.
 ;;;; URL: http://github.com/toshiabe/unset-macro-character
 ;;;; Copyright (c) 2011 Toshihisa Abe <toshihisa.abe@gmail.com>
-;;;; Licence: MIT License
+;;;; License: MIT License
 
 (in-package #:unset-macro-character)
 
 (defun unset-macro-character (#1=char &optional (#2=*readtable* *readtable*))
   "Unset macro-character function from given readtable."
-  (set-syntax-from-char #1# nilc #2# (copy-readtable nil)))
+  (set-syntax-from-char #1# #1# #2# (copy-readtable nil)))
 
 (defun unset-dispatch-macro-character (#1=dispatch-char #2=sub-char &optional (#3=*readtable* *readtable*))
   "Unset dispatch-macro-character-function from given readtable."
-  ;; (setf (get-dispatch-macro-character #1# #2# #3#) nil)
   (set-dispatch-macro-character #1# #2# (get-dispatch-macro-character #\# illc (copy-readtable nil)) #3#))
 
 ;;; complement of `make-dispatch-macro-character'
