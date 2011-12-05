e44f86930e0ae4fc858573a4acd8f6a8356bebf2
toshiabe <toshihisa.abe@gmail.com>
2011-12-05 23:48:45 +0900

    lisp-unit -> fiveam.

--- t/remove-dispatch-macro-character.lisp
+++ t/remove-dispatch-macro-character.lisp
@@ -1,68 +1,77 @@
 ;;;; remove-dispatch-macro-character.lisp
 
 ;;; testing remove-dispatch-macro-character.lisp
+;;; @todo: too complex
 
 (in-package #:unset-macro-character.test)
 
-(define-test #:remove-dispatch-macro-character.current
+(5am:in-suite :dispatch-macro-character)
+
+(5am:test :remove.current
   (let ((*readtable* #1=(copy-readtable nil)))
 
     (make-dispatch-macro-character #\$ nil)
     (multiple-value-bind (f nt) (get-macro-character #\$ *readtable*)
-      #+(or sbcl cmu ccl ecl)
-      (assert-eql (values (get-macro-character #\# #1#) nil)
-                  (values f nt))
-      #+clisp
-      (assert-eql (values t nil) (values (functionp f) nt))
+      #+(or SBCL CMU CCL ECL)
+      (5am:is (equal (multiple-value-list (values (get-macro-character #\# #1#) nil))
+                     (multiple-value-list (values f nt))))
+      #+CLISP
+      (5am:is (equal (multiple-value-list (values t nil))
+                     (multiple-value-list (values (functionp f) nt))))
       )
 
-    (make-dispatch-macro-character #\% t)
+    (make-dispatch-macro-character #\% t *readtable*)
     (multiple-value-bind (fn nt) (get-macro-character #\% *readtable*)
-      #+(or sbcl cmu ccl ecl)
-      (assert-eql (values (get-macro-character #\# #1#) t)
-                  (values fn nt))
-      #+clisp
-      (assert-eql (values t t) (values (functionp fn) nt))
+      #+(or SBCL CMU CCL ECL)
+      (5am:is (equal (multiple-value-list (values (get-macro-character #\# #1#) t))
+                     (multiple-value-list (values fn nt))))
+      #+CLISP
+      (5am:is (equal (multiple-value-list (values t t))
+                     (multiple-value-list (values (functionp fn) nt))))
       )
 
     (remove-dispatch-macro-character #\$)
     (remove-dispatch-macro-character #\%)
 
     (multiple-value-bind (f nt) (get-macro-character #\$ *readtable*)
-      (assert-eql (get-macro-character #\$ #1#) (values f nt)))
+      (5am:is (equal (multiple-value-list (get-macro-character #\$ #1#))
+                     (multiple-value-list (values f nt)))))
 
     (multiple-value-bind (f nt) (get-macro-character #\% *readtable*)
-      (assert-eql (get-macro-character #\% #1#) (values f nt)))))
+      (5am:is (equal (multiple-value-list (get-macro-character #\% #1#))
+                     (multiple-value-list (values f nt)))))))
 
-(define-test #:remove-dispatch-macro-character.saved-readtable
+(5am:test :remove.saved
   (let ((rt #1=(copy-readtable nil)))
 
     (make-dispatch-macro-character #\$ nil rt)
-
     (multiple-value-bind (f nt) (get-macro-character #\$ rt)
-      #+(or sbcl cmu ccl ecl)
-      (assert-eql (values (get-macro-character #\# #1#) nil)
-                  (values f nt))
-      #+clisp
-      (assert-eql (values t nil) (values (functionp f) nt))
+      #+(or SBCL CMU CCL ECL)
+      (5am:is (equal (multiple-value-list (values (get-macro-character #\# #1#) nil))
+                     (multiple-value-list (values f nt))))
+      #+CLISP
+      (5am:is (equal (multiple-value-list (values t nil))
+                     (multiple-value-list (values (functionp f) nt))))
       )
 
-
     (make-dispatch-macro-character #\% t rt)
-
-    (multiple-value-bind (f nt) (get-macro-character #\% rt)
-      #+(or sbcl cmu ccl ecl)
-      (assert-eql (values (get-macro-character #\# #1#) t)
-                  (values f nt))
-      #+clisp (assert-eql (values t t) (values (functionp f) nt))
+    (multiple-value-bind (fn nt) (get-macro-character #\% rt)
+      #+(or SBCL CMU CCL ECL)
+      (5am:is (equal (multiple-value-list (values (get-macro-character #\# #1#) t))
+                     (multiple-value-list (values fn nt))))
+      #+CLISP
+      (5am:is (equal (multiple-value-list (values t t))
+                     (multiple-value-list (values (functionp fn) nt))))
       )
 
     (remove-dispatch-macro-character #\$ rt)
     (remove-dispatch-macro-character #\% rt)
 
-    (multiple-value-bind (f nt) (get-macro-character #\$ rt)
-      (assert-eql (get-macro-character #\$ #1#) (values f nt)))
+    (multiple-value-bind (fn nt) (get-macro-character #\$ rt)
+      (5am:is (equal (multiple-value-list (get-macro-character #\$ #1#))
+                     (multiple-value-list (values fn nt)))))
 
-    (multiple-value-bind (f nt) (get-macro-character #\% rt)
-      (assert-eql (get-macro-character #\% #1#) (values f nt)))))
+    (multiple-value-bind (e nt) (get-macro-character #\% rt)
+      (5am:is (equal (multiple-value-list (get-macro-character #\% #1#))
+                     (multiple-value-list (values e nt)))))))
 
