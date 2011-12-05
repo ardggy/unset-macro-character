11d0501b40490b88dfb1867e59f6091179937c96
toshiabe <toshihisa.abe@gmail.com>
2011-12-05 23:49:02 +0900

    lisp-unit -> fiveam.

--- t/unset-dispatch-macro-character.lisp
+++ t/unset-dispatch-macro-character.lisp
@@ -4,18 +4,20 @@
 
 (in-package #:unset-macro-character.test)
 
-(define-test #:unset-dispatch-macro-character.current
+(5am:in-suite :dispatch-macro-character)
+
+(5am:test :unset.current
   (let ((*readtable* #1=(copy-readtable nil)))
     (set-dispatch-macro-character #\# #\%
                                   (get-dispatch-macro-character #\# #\' *readtable*))
 
     (unset-dispatch-macro-character #\# #\%)
 
-    (assert-eql (get-dispatch-macro-character #\# #\%)
-                (get-dispatch-macro-character #\# #\NEWLINE #1#))))
+    (5am:is (equal (multiple-value-list (get-dispatch-macro-character #\# #\%))
+                   (multiple-value-list (get-dispatch-macro-character #\# #\NEWLINE #1#))))))
 
 
-(define-test #:unset-dispatch-macro-character.saved-readtable
+(5am:test :unset.saved
   (let ((*readtable* #1=(copy-readtable nil))
         (rt #1#))
     (set-dispatch-macro-character #\# #\%
@@ -24,6 +26,6 @@
 
     (unset-dispatch-macro-character #\# #\% rt)
 
-    (assert-eql (get-dispatch-macro-character #\# #\% rt)
-                (get-dispatch-macro-character #\# #\NEWLINE #1#))))
+    (5am:is (equal (multiple-value-list (get-dispatch-macro-character #\# #\% rt))
+                   (multiple-value-list (get-dispatch-macro-character #\# #\NEWLINE #1#))))))
 
