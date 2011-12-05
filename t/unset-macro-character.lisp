7fd73f8cee88d12cd53590325aa018e45fea0958
toshiabe <toshihisa.abe@gmail.com>
2011-12-05 23:48:28 +0900

    lisp-unit -> fiveam.

--- t/unset-macro-character.lisp
+++ t/unset-macro-character.lisp
@@ -1,25 +1,28 @@
 ;;;; test
 
 ;;; testing unset-macro-character
+;;; *readtable* and lexical bound readtable
 
 (in-package #:unset-macro-character.test)
 
-(define-test #:unset-macro-character.current
+(5am:in-suite :macro-character)
+
+(5am:test :unset.current
   (let ((*readtable* #1=(copy-readtable nil)))
     (set-macro-character #\[ (get-macro-character #\( ))
     (unset-macro-character #\[)
 
     (multiple-value-bind (f nt) (get-macro-character #\[)
-      (assert-eql (get-macro-character #\[ #1#)
-                  (values f nt)))))
+      (5am:is (equal (multiple-value-list (get-macro-character #\[ #1#))
+                     (multiple-value-list (values f nt)))))))
 
-(define-test #:unset-macro-character.saved
+(5am:test :unset.saved
   (let ((rt #1=(copy-readtable nil))
         (*readtable* #1#))
     (set-macro-character #\[ (get-macro-character #\( ) nil rt)
     (unset-macro-character #\[ rt)
 
     (multiple-value-bind (f nt) (get-macro-character #\[ rt)
-      (assert-eql (get-macro-character #\[ #1#)
-                  (values f nt)))))
+      (5am:is (equal (multiple-value-list (get-macro-character #\[ rt))
+                     (multiple-value-list (values f nt)))))))
 
